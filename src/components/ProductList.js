04e9c5c40d232eadf46537b1edd28a2ce934e6be
Miguel <99726342+mig-code@users.noreply.github.com>
2022-10-24 18:46:33 +0200

    Adding functionality and style to components

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState, useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import Item from './Item';
+import '../styles/ProductList.scss';
 
 export default function ProductList() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const { mobilesData } = useContext(AppContext);
+  console.log(mobilesData);
+  const products = mobilesData;
+
+  const handleChange = event => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
-    <div>ProductList</div>
-  )
+    <>
+      <div className='search-container'>
+        <input
+          className='search-placeholder'
+          type='text'
+          placeholder=''
+          value={searchQuery}
+          onChange={e => handleChange(e)}
+        />
+
+        <div className='search-grid'>
+          {products
+            .filter(item => {
+              return (
+                item.brand.toLowerCase().includes(searchQuery.toLowerCase()) ||
+                item.model.toLowerCase().includes(searchQuery.toLowerCase())
+              );
+            })
+            .map(product => (
+              <div key={product.id}>
+                <Item product={product} />
+              </div>
+            ))}
+        </div>
+      </div>
+    </>
+  );
 }
