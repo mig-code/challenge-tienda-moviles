8304fd71abd4ef8e72d0837077ae2244c3724d8e
Miguel <99726342+mig-code@users.noreply.github.com>
2022-10-24 13:41:00 +0200

    Link sass and fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import '../styles/Header.scss'
 
 export default function Header() {
-  return (
-    <div>
-        <h1>Mobile Shop</h1>
-        <p>Breadcumbs</p>
-        <p>Cart</p>
-    </div>
-  )
+    return (
+        <div className='header'>
+            <h1>Mobile Shop</h1>
+            <p>Breadcumbs</p>
+            <p>Cart</p>
+        </div>
+    )
 }
