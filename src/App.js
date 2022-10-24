20b7416d7c5872147c140f9a25c41b966545ec62
Miguel <99726342+mig-code@users.noreply.github.com>
2022-10-24 17:07:27 +0200

    Config Routes and basic Header style

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './styles/App.scss';
 import Header from './components/Header';
 import ProductList from './components/ProductList';
 import ProductDetails from './components/ProductDetails';
+import { Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <ProductList />
-      <ProductDetails />
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/details/" element={<ProductDetails />} />
+      </Routes>
 
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import '../styles/Header.scss'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Header.scss';
+import cart from '../assets/cart.svg';
 
 export default function Header() {
-    return (
-        <div className='header'>
-            <h1>Mobile Shop</h1>
-            <p>Breadcumbs</p>
-            <p>Cart</p>
-        </div>
-    )
+  return (
+    <div className='header'>
+      <div className='header__left'>
+        <Link to='/'>
+          <h1 className='header__title'>Mobile Shop</h1>
+        </Link>
+        <Link to='/'>
+          <p className='header__breadcumbs'>Inicio - </p>
+        </Link>
+      </div>
+      <div className='header__right'>
+        <img className='header__cart' src={cart} alt='' />
+        <p className='header__cart-count'>3</p>
+      </div>
+    </div>
+  );
 }
