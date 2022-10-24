ad5cc502694029966bba477f0a2175dcb6e74670
Miguel <99726342+mig-code@users.noreply.github.com>
2022-10-24 18:45:44 +0200

    Adding functionality and style to components

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -5,9 +5,27 @@ export const AppContext = createContext();
 const AppProvider = props => {
   console.log('LOAD PROVIDER');
   const [cartCount, setCartCount] = useState(3);
+  const [mobilesData, setMobilesData] = useState([]);
+
+  function getMobilesData() {
+    fetch('https://front-test-api.herokuapp.com/api/product')
+      .then(response => response.json())
+      .then(data => {
+        setMobilesData(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+      
+  }
+
+  useEffect(() => {
+    console.log('LOAD PROVIDER one time');
+    getMobilesData();
+    }, []);
 
   return (
-    <AppContext.Provider value={{ cartCount, setCartCount }}>
+    <AppContext.Provider value={{ cartCount, setCartCount,mobilesData }}>
       {props.children}
     </AppContext.Provider>
   );
