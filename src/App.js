069887d1e01ef2f5fc5ca40979cfdd3ee9e0e5fd
Miguel <99726342+mig-code@users.noreply.github.com>
2022-10-24 12:21:09 +0200

    Config Eslint and Prettier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-
 import './styles/App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Technical Challenge</h1>
       <h2>Tienda de móviles</h2>
     </div>
