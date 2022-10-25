8ee9bbc19996b987c340e396fe0cd15fcdac7ed5
Miguel <99726342+mig-code@users.noreply.github.com>
2022-10-25 14:17:41 +0200

    Addign user optiones

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -8,12 +8,45 @@ export default function DetailsView(props) {
   const { mobileId } = useParams();
   const [mobileData, setMobileData] = useState(null);
   const { cartCount, setCartCount, setBreadcumbName } = useContext(AppContext);
+  const [userColor, setUserColor] = useState(1);
+  const [userStorage, setUserStorage] = useState(1);
+
+  function handleColor(code) {
+    if (code === 1000) {
+      setUserColor(1);
+    }
+    if (code === 1001) {
+      setUserColor(2);
+    }
+    if (code === 1002) {
+      setUserColor(3);
+    }
+    if (code === 1003) {
+      setUserColor(4);
+    }
+    console.log(userColor);
+  }
+  function handleStorage(code) {
+    if (code === 2000) {
+      setUserStorage(1);
+    }
+    if (code === 2001) {
+      setUserStorage(2);
+    }
+    if (code === 2002) {
+      setUserStorage(3);
+    }
+    if (code === 2003) {
+      setUserStorage(4);
+    }
+
+  }
 
   const addItems = async () => {
     const formData = {
-      id: 'cGjFJlmqNPIwU59AOcY8H',
-      colorCode: 1,
-      storageCode: 1,
+      id: mobileId,
+      colorCode: userColor,
+      storageCode: userStorage,
     };
 
     fetch('https://front-test-api.herokuapp.com/api/cart', {
@@ -24,6 +57,7 @@ export default function DetailsView(props) {
     })
       .then(response => response.text())
       .then(result => {
+        console.log(formData);
         console.log('Success:', result);
         localStorage.setItem('Cart Count', parseInt(cartCount) + 1);
         setCartCount(localStorage.getItem('Cart Count'));
@@ -38,15 +72,13 @@ export default function DetailsView(props) {
     fetch(`https://front-test-api.herokuapp.com/api/product/${mobileId}`)
       .then(response => response.json())
       .then(data => {
-       
         console.log(data);
-        console.log(typeof(data));
         setMobileData(data);
         setBreadcumbName(' - ' + data.model);
       })
       .catch(error => {
         console.error('Error:', error);
-        console.log("HERE")
+        console.log('HERE');
       });
   }
 
@@ -56,9 +88,9 @@ export default function DetailsView(props) {
 
   return (
     <>
-      {(!mobileData || mobileData.message ) ? (
+      {!mobileData || mobileData.message ? (
         <div className='details-container'>
-          {mobileData && <PageNotFound/>}
+          {mobileData && <PageNotFound />}
         </div>
       ) : (
         <div className='details-container'>
@@ -72,7 +104,6 @@ export default function DetailsView(props) {
           <div className='details-right'>
             <h1 className='details-right__model'>{mobileData.model} </h1>
             <div className='details-right__brand'>
-              {' '}
               Marca: {mobileData.brand}{' '}
             </div>
 
@@ -81,37 +112,49 @@ export default function DetailsView(props) {
             <p className='details-right__feature'> RAM: {mobileData.ram}</p>
             <p className='details-right__feature'> OS: {mobileData.os}</p>
             <p className='details-right__feature'>
-              {' '}
               Resolución: {mobileData.displayResolution}
             </p>
             <p className='details-right__feature'>
-              {' '}
               Batería: {mobileData.battery}
             </p>
             <p className='details-right__feature'>
-              {' '}
               Cámara Principal: {mobileData.primaryCamera[0]}
             </p>
             <p className='details-right__feature'>
-              {' '}
               Cámara Secundaria: {mobileData.secondaryCmera}
             </p>
             <p className='details-right__feature'>
-              {' '}
               Tamaño: {mobileData.dimentions}
             </p>
             <p className='details-right__feature'>
-              {' '}
               Peso: {mobileData.weight} g
             </p>
 
-            <div className='details-actions'>
+            <div className='actions'>
               <h1>ACCIONES</h1>
+              <div className='actions__colors-box'>
+                {mobileData.options.colors.map(item => (
+                  <div
+                    onClick={() => handleColor(item.code)}
+                    key={item.code}
+                    className='actions__color-option'
+                  >
+                    {item.name}
+                  </div>
+                ))}
+              </div>
+              <div className='actions__storage-box'>
+                {mobileData.options.storages.map(item => (
+                  <div
+                    onClick={() => handleStorage(item.code)}
+                    key={item.code}
+                    className='actions__storage-option'
+                  >
+                    {item.name}
+                  </div>
+                ))}
+              </div>
               <Link to={`/`}>
-                <div className='details-right__colors'>
-                  {' '}
-                  colors: {mobileData.colors.length}{' '}
-                </div>
                 <button className='details-right__button-back'>VOLVER</button>
               </Link>
               <button onClick={addItems}>AÑADIR AL CARRO</button>
