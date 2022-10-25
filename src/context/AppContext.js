f9917fff15ee84c3035aaecfbefab30dae28d60c
Miguel <99726342+mig-code@users.noreply.github.com>
2022-10-25 12:04:54 +0200

    ADD CACHE SYSTEM

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -5,26 +5,35 @@ export const AppContext = createContext();
 const AppProvider = props => {
   console.log('LOAD PROVIDER');
 
- 
   const [cartCount, setCartCount] = useState(0);
   const [mobilesData, setMobilesData] = useState([]);
-  const [breadcumbName, setBreadcumbName] = useState('Movil');
+  const [breadcumbName, setBreadcumbName] = useState('');
+  const [startCacheTime, setStartCacheTime] = useState(Date.now());
+  const [dataIsinCache, setDataIsinCache] = useState(false);
 
   function getMobilesData() {
-    fetch('https://front-test-api.herokuapp.com/api/product')
-      .then(response => response.json())
-      .then(data => {
-        setMobilesData(data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+    if (!dataIsinCache) {
+      fetch('https://front-test-api.herokuapp.com/api/product')
+        .then(response => response.json())
+        .then(data => {
+          localStorage.setItem('Mobiles Data', JSON.stringify(data));
+          setMobilesData(JSON.parse(localStorage.getItem('Mobiles Data')));
+          setDataIsinCache(true);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
+  }
+
+  if ((Date.now() - startCacheTime) / 1000 > 3600) {
+    setStartCacheTime(Date.now());
+    setDataIsinCache(false);
   }
 
   useEffect(() => {
-    console.log('LOAD PROVIDER one time');
     getMobilesData();
-  }, []);
+  }, [dataIsinCache]);
 
   return (
     <AppContext.Provider
